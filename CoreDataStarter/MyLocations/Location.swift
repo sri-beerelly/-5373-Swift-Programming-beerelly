f18f47caf34b1efa4d998ce6a38e90d67716426f
S. Berrelly <sberrelly@bo120-08-46818.local>
2015-04-10 13:34:43 -0500

    assignment 5

---
+++ CoreDataStarter/MyLocations/Location.swift
@@ -0,0 +1,22 @@
+//
+//  MyLocation.swift
+//  MyLocations
+//
+//  Created by S. Berrelly on 4/10/15.
+//  Copyright (c) 2015 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import CoreLocation
+
+class Location: NSManagedObject {
+
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var date: NSDate
+    @NSManaged var locationDescription: String
+    @NSManaged var category: String
+    @NSManaged var placemark: CLPlacemark?
+
+}
